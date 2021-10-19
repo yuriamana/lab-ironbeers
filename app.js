867ffe4314528ae2fe1776aa111d70e630f6e42e
Yuki Kato <katoyuki0113@gmail.com>
2021-10-19 21:26:34 +0200

    get all beers and random beer ok but no styling

--- app.js
+++ app.js
@@ -13,13 +13,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beers => {
+      // console.log(beers)
+      res.render('beers.hbs', { beers });
+    })
+    .catch(error => console.error('error'));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      res.render('random-beer.hbs', { randomBeer });
+    })
+    .catch(error => console.error('error'));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
